f7320f2b8b1d6cc0c93cd6add5738b31be785b22
Pedro Joaquim Araujo <pedroa990@gmail.com>
2024-06-03 23:02:16 -0300

    Concluído protótipo herosection desktop

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -4,7 +4,7 @@ import Banner from "../Banner";
 const HeroSection = () => {
   return (
     <section className="bg-pcream h-[720px] flex justify-center items-center">
-      <div>
+      <div className="absolute left-[420px]">
         <Image
           src="/foto1.png"
           width={573}
@@ -12,7 +12,7 @@ const HeroSection = () => {
           alt="Foto de Marina Araujo, uma mulher negra, sorrindo, com cabelos cacheados, usando um vestido roxo e marrom, com um fundo de estante e  folhas verdes."
         />
       </div>
-      <div>
+      <div className="absolute right-[410px]">
         <Banner
           title="Psicanálise para uma Jornada Racial Positiva"
           text="Explore a jornada para o autoconhecimento e cura emocional. Agende sua sessão e inicie sua jornada de transformação hoje mesmo com uma psicanalista dedicada a questões raciais."
