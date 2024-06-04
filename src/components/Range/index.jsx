78d12b2beb2e16a58e365d323bef2ac15c859c2a
Pedro Joaquim Araujo <pedroa990@gmail.com>
2024-06-03 23:02:02 -0300

    Concluído protótipo range desktop

---
+++ src/components/Range/index.jsx
@@ -0,0 +1,7 @@
+const Range = () => {
+    return (
+        <div className="flex justify-center items-center w-1/1 h-[170px] bg-fita bg-no-repeat bg-cover"><p className="text-[32px] text-pcream font-cormorant">&quot;Transforme dor em libertação. Juntos, criaremos um futuro de cura e empoderamento.&quot;</p></div>
+    )
+}
+
+export default Range
