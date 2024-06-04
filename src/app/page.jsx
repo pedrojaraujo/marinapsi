43f929f346f45e2322f11061c6c63e950c0f71c2
Pedro Joaquim Araujo <pedroa990@gmail.com>
2024-06-03 23:01:35 -0300

    Concluido prototipo desktop banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,17 @@
+import AboutMe from "@/components/AboutMe";
 import Header from "@/components/Header";
+import Help from "@/components/Help";
 import HeroSection from "@/components/HeroSection";
+import Range from "@/components/Range";
 
 export default function Home() {
   return (
     <main>
       <Header id="inicio" />
       <HeroSection />
+      <Range />
+      <Help />
+      <AboutMe />
     </main>
   );
 }
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Button from "../Button";
 
 const Banner = ({ title, text }) => {
   return (
-    <div className=" bg-pred w-[775px] h-[368px] rounded-[16px] opacity-75 backdrop-blur-sm ">
-      <div className="">
-        {title}
-        {text}
+    <div className="relative w-[775px] h-[368px] rounded-[16px] backdrop-blur-sm flex items-center p-10 text-pcream">
+      <div className="absolute inset-0 bg-pred opacity-75 rounded-[16px]"></div>
+      <div className="relative font-trebuchet w-4/5">
+        <h1 className="text-2xl py-2">{title}</h1>
+        <p className="py-5">{text}</p>
+        <Button text="Converse comigo!" href="#contato"
+          width="194px" height="36px" />
       </div>
     </div>
   );
